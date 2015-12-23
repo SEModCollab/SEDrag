55154cffdfa1ee4219eeb4e9593fa8cd7974369e
DraygoKorvan <draygokorvan@ps-roughriders.com>
2015-12-22 19:38:30 -0600

    Updated HeatDefinition to fix crash on save. Updating loading.

--- HeatDefinition/HeatDefinition.cs
+++ HeatDefinition/HeatDefinition.cs
@@ -14,6 +14,7 @@ using IMySlimBlock = Sandbox.ModAPI.IMySlimBlock;
 using Sandbox.ModAPI.Ingame;
 using Sandbox.ModAPI.Interfaces;
 using System.Text.RegularExpressions;
+using System.Runtime.Serialization;
 
 namespace SEDrag.Definition
 {
@@ -26,7 +27,6 @@ namespace SEDrag.Definition
 			get { return m_data; }
 			set { m_data = value; }
 		}
-
 		public void Init()
 		{
 			//init
@@ -40,8 +40,8 @@ namespace SEDrag.Definition
 				try
 				{
 
-					Log.Info(keys.Id.SubtypeName);
-					Log.Info(keys.DescriptionString);
+					Log.DebugWrite(DragSettings.DebugLevel.Info,keys.Id.SubtypeName);
+					Log.DebugWrite(DragSettings.DebugLevel.Info,keys.DescriptionString);
 					if (keys.DescriptionString == null || keys.DescriptionString.Length == 0) continue;
 					var res = reg.Split(keys.DescriptionString);
 					Log.IncreaseIndent();
@@ -133,6 +133,35 @@ namespace SEDrag.Definition
 												break;
 										}
 										break;
+									case "top":
+										switch (dataeq[1].ToLower())
+										{
+											case "u":
+											case "up":
+												value.top = Base6Directions.Direction.Up;
+												break;
+											case "d":
+											case "down":
+												value.top = Base6Directions.Direction.Down;
+												break;
+											case "l":
+											case "left":
+												value.top = Base6Directions.Direction.Left;
+												break;
+											case "r":
+											case "right":
+												value.top = Base6Directions.Direction.Right;
+												break;
+											case "f":
+											case "forward":
+												value.top = Base6Directions.Direction.Forward;
+												break;
+											case "b":
+											case "backward":
+												value.top = Base6Directions.Direction.Backward;
+												break;
+										}
+										break;
 									case "tall":
 										value.heatThresh_u = Convert.ToDouble(dataeq[1]);
 										value.heatThresh_d = Convert.ToDouble(dataeq[1]);
@@ -167,6 +196,7 @@ namespace SEDrag.Definition
 										break;
 								}
 							}
+							Log.DebugWrite(DragSettings.DebugLevel.Info, string.Format("Adding {0}", keys.Id.SubtypeName));
 							data.Add(keys.Id.SubtypeName, value);
 						}
 					}
@@ -175,7 +205,7 @@ namespace SEDrag.Definition
 				}
 				catch (Exception ex)
 				{
-					Log.DebugWrite(DragSettings.DebugLevel.Info, string.Format("Warning Error in Description: {0} {1}", keys.Id.SubtypeName, ex.ToString()));
+					Log.DebugWrite(DragSettings.DebugLevel.Error, string.Format("Warning Error in Description: {0} {1}", keys.Id.SubtypeName, ex.ToString()));
 				}
 
 			}
@@ -184,8 +214,6 @@ namespace SEDrag.Definition
 				Log.DebugWrite(DragSettings.DebugLevel.Info, string.Format("{0} updated.", items.Key));
 			}
 		}
-
-
 		public void Load(bool l_default = false)
 		{
 			Log.DebugWrite(DragSettings.DebugLevel.Info, "Loading XML");
@@ -198,13 +226,14 @@ namespace SEDrag.Definition
 			}
 			try
 			{
-				if (MyAPIGateway.Utilities.FileExistsInLocalStorage(FILE, typeof(HeatDefinition)) && !l_default)
+				if (MyAPIGateway.Utilities.FileExistsInLocalStorage(FILE, typeof(HeatContainerWrapper)) && !l_default)
 				{
-					var reader = MyAPIGateway.Utilities.ReadFileInLocalStorage(FILE, typeof(HeatDefinition));
+					var reader = MyAPIGateway.Utilities.ReadFileInLocalStorage(FILE, typeof(HeatContainerWrapper));
 					var xmlText = reader.ReadToEnd();
 					reader.Close();
-					m_data.Clear();
-					m_data = MyAPIGateway.Utilities.SerializeFromXML<HeatDefinition>(xmlText).m_data;
+					//m_data.Clear();
+					var savecontainer = MyAPIGateway.Utilities.SerializeFromXML<HeatContainerWrapper>(xmlText);
+					loadContainer(savecontainer);
 					Log.DebugWrite(DragSettings.DebugLevel.Info, "Load Complete");
 					return;
 				}
@@ -214,14 +243,58 @@ namespace SEDrag.Definition
 				Log.DebugWrite(DragSettings.DebugLevel.Error, ex);
 			}
 		}
+
+		private void loadContainer(HeatContainerWrapper savecontainer)
+		{
+			HeatData trash = new HeatData();
+			foreach (HeatContainer savedata in savecontainer.wrapper)
+			{
+				
+				if(data.TryGetValue(savedata.subtypeid, out trash))
+				{
+					data.Remove(savedata.subtypeid);
+				}
+				data.Add(savedata.subtypeid, savedata.value);
+			}
+		}
+
 		public void Save()
 		{
 			Log.DebugWrite(DragSettings.DebugLevel.Info, "Saving XML (heat)");
-			var writer = MyAPIGateway.Utilities.WriteFileInLocalStorage(FILE, typeof(HeatDefinition));
-			writer.Write(MyAPIGateway.Utilities.SerializeToXML(this));
+			HeatContainerWrapper savecontainer = new HeatContainerWrapper();
+			foreach(KeyValuePair<string, HeatData> kpair in data)
+			{
+				Log.DebugWrite(DragSettings.DebugLevel.Info, kpair.Key);
+				savecontainer.wrapper.Add(new HeatContainer(kpair.Key, kpair.Value));
+			}
+			var writer = MyAPIGateway.Utilities.WriteFileInLocalStorage(FILE, typeof(HeatContainerWrapper));
+			writer.Write(MyAPIGateway.Utilities.SerializeToXML(savecontainer));
 			writer.Flush();
 			writer.Close();
 			Log.DebugWrite(DragSettings.DebugLevel.Info, "Save Complete");
 		}
+		public class HeatContainerWrapper
+		{
+			public List<HeatContainer> wrapper = new List<HeatContainer>();
+			public HeatContainerWrapper()
+			{
+
+			}
+		}
+		public class HeatContainer
+		{
+			public string subtypeid;
+			public HeatData value;
+			public HeatContainer()
+			{
+				subtypeid = "";
+			}
+			public HeatContainer(string key, HeatData value)
+			{
+				subtypeid = key;
+				this.value = value;
+			}
+		}
+
 	}
 }
